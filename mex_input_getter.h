e259430c8d40480ac43467ba68f9460e083ad98c
Diederick Niehorster <dcnieho@gmail.com>
2021-03-10 22:01:11 +0100

    mex_input_getter now also supports std::pair and std::tuple. cleaned up some code in the process

--- mex_input_getter.h
+++ mex_input_getter.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <optional>
 #include <string>
+#include <utility>
 #include <tuple>
 #include <string_view>
 #include <functional>
@@ -37,69 +38,104 @@ namespace mxTypes
             return std::to_string(number) + suffix;
         }
 
+        // forward declaration
+        template <typename OutputType>
+        constexpr std::string buildCorrespondingMatlabTypeString_impl();
+        // end forward declaration
         template <typename OutputType, bool IsContainer>
-        constexpr std::tuple<const char*, bool> buildCorrespondingMatlabTypeString_impl()
+        constexpr std::string buildCorrespondingMatlabTypeString_impl()
         {
             if constexpr (std::is_same_v<OutputType, std::string>)
+            {
                 if constexpr (IsContainer)
-                    return { "cellstring",true };
+                    return "cellstring";
                 else
-                    return { "string",true };
+                    return "string";
+            }
             else
             {
                 constexpr mxClassID mxClass = typeToMxClass_v<OutputType>;
-                return { mxClassToString<mxClass>(),false };
+                return mxClassToString<mxClass>();
             }
+        }
+        template <bool IsContainer, template <class...> class TP, class... Args, size_t... Is>
+        constexpr std::string buildCorrespondingMatlabTypeString_impl(TP<Args...>&&, std::index_sequence<Is...>)
+        {
+            std::string outStr;
+            outStr.reserve(64);
+            if constexpr (IsContainer)
+                outStr = "M";
+            else
+                outStr = "1";
+            outStr += "x" + std::to_string(sizeof...(Args)) + " cell array ";
+            if constexpr (IsContainer)
+                outStr += "with each row containing ";
+            else
+                outStr += "of types ";
+            outStr += "{";
+
+            std::size_t length = sizeof...(Args);
+            ((outStr += buildCorrespondingMatlabTypeString_impl<Args>() + (Is == length-1 ? "" : ", ")),...);
+            outStr += "}";
+
+            return outStr;
         }
 
         template <typename OutputType>
-        constexpr std::tuple<const char*, bool, bool> buildCorrespondingMatlabTypeString_impl(std::string_view funcID_, size_t idx_, size_t offset_)
+        constexpr std::string buildCorrespondingMatlabTypeString_impl()
         {
             if constexpr (is_container_v<OutputType> && !std::is_same_v<OutputType, std::string>)
             {
-                auto [typeStr, special] = buildCorrespondingMatlabTypeString_impl<typename OutputType::value_type, true>();
-                return { typeStr, special, true };
+                if constexpr (is_specialization_v<typename OutputType::value_type, std::tuple> || is_specialization_v<typename OutputType::value_type, std::pair>)
+                {
+                    using theTuple = typename OutputType::value_type;
+                    return buildCorrespondingMatlabTypeString_impl<true>(theTuple(), std::make_index_sequence<std::tuple_size_v<theTuple>>{});
+                }
+                else
+                    return buildCorrespondingMatlabTypeString_impl<typename OutputType::value_type, true>();
             }
             else
             {
-                auto [typeStr, special] = buildCorrespondingMatlabTypeString_impl<OutputType, false>();
-                return { typeStr, special, false };
+                if constexpr (is_specialization_v<OutputType, std::tuple> || is_specialization_v<OutputType, std::pair>)
+                    return buildCorrespondingMatlabTypeString_impl<false>(OutputType(), std::make_index_sequence<std::tuple_size_v<OutputType>>{});
+                else
+                    return buildCorrespondingMatlabTypeString_impl<OutputType, false>();
             }
         }
 
         // if converter provided, for scalar output
         template <typename T, typename Arg>
-        constexpr std::tuple<const char*, bool, bool> buildCorrespondingMatlabTypeString(std::string_view funcID_, size_t idx_, size_t offset_, T(*conv_)(Arg))
+        constexpr std::string buildCorrespondingMatlabTypeString(T(*conv_)(Arg))
         {
-            return buildCorrespondingMatlabTypeString_impl<std::decay_t<Arg>>(funcID_, idx_, offset_);
+            return buildCorrespondingMatlabTypeString_impl<std::decay_t<Arg>>();
         }
         template <typename T, typename Arg>
-        constexpr std::tuple<const char*, bool, bool> buildCorrespondingMatlabTypeString(std::string_view funcID_, size_t idx_, size_t offset_, std::function<T(Arg)> conv_)
+        constexpr std::string buildCorrespondingMatlabTypeString(std::function<T(Arg)> conv_)
         {
-            return buildCorrespondingMatlabTypeString_impl<std::decay_t<Arg>>(funcID_, idx_, offset_);
+            return buildCorrespondingMatlabTypeString_impl<std::decay_t<Arg>>();
         }
         // if converter provided, for container output
         template <typename T, typename Arg>
-        constexpr std::tuple<const char*, bool, bool> buildCorrespondingMatlabTypeString(std::string_view funcID_, size_t idx_, size_t offset_, typename T::value_type(*conv_)(Arg))
+        constexpr std::string buildCorrespondingMatlabTypeString(typename T::value_type(*conv_)(Arg))
         {
-            return buildCorrespondingMatlabTypeString_impl<replace_specialization_type_t<std::vector<T::value_type>, std::decay_t<Arg>>>(funcID_, idx_, offset_);
+            return buildCorrespondingMatlabTypeString_impl<replace_specialization_type_t<std::vector<T::value_type>, std::decay_t<Arg>>>();
         }
         template <typename T, typename Arg>
-        constexpr std::tuple<const char*, bool, bool> buildCorrespondingMatlabTypeString(std::string_view funcID_, size_t idx_, size_t offset_, std::function<typename T::value_type(Arg)> conv_)
+        constexpr std::string buildCorrespondingMatlabTypeString(std::function<typename T::value_type(Arg)> conv_)
         {
-            return buildCorrespondingMatlabTypeString_impl<replace_specialization_type_t<std::vector<T::value_type>, std::decay_t<Arg>>>(funcID_, idx_, offset_);
+            return buildCorrespondingMatlabTypeString_impl<replace_specialization_type_t<std::vector<T::value_type>, std::decay_t<Arg>>>();
         }
         // if no converter provided by user
         template <typename T>
-        constexpr std::tuple<const char*, bool, bool> buildCorrespondingMatlabTypeString(std::string_view funcID_, size_t idx_, size_t offset_, nullptr_t conv_)
+        constexpr std::string buildCorrespondingMatlabTypeString(nullptr_t conv_)
         {
-            return buildCorrespondingMatlabTypeString_impl<T>(funcID_, idx_, offset_);
+            return buildCorrespondingMatlabTypeString_impl<T>();
         }
 
         template <typename T, typename Converter>
         void buildAndThrowError(std::string_view funcID_, size_t idx_, size_t offset_, bool isOptional_, Converter conv_)
         {
-            auto [typeStr, special, isContainer] = buildCorrespondingMatlabTypeString<T>(funcID_, idx_, offset_, conv_);
+            auto typeStr = buildCorrespondingMatlabTypeString<T>(conv_);
             std::string out;
             out.reserve(50);
             out += "SWAG::";
@@ -114,10 +150,21 @@ namespace mxTypes
             out += NumberToOrdinal(ordinal) + " argument must be a";
             if (typeStr[0] == 'a' || typeStr[0] == 'e' || typeStr[0] == 'i' || typeStr[0] == 'o' || typeStr[0] == 'u')
                 out += "n";
-            out += " " << typeStr;
+            out += " " + typeStr;
+
+            // if simple type (e.g. int) or container of simple type (e.g. std::vector<int>),
+            // automatically add "scalar" or "array" to the string
+            bool special = true;
+            if constexpr (std::is_arithmetic_v<T>)
+                special = false;
+            else if constexpr (is_container_v<T> && !std::is_same_v<T, std::string>)
+            {
+                if constexpr (std::is_arithmetic_v<typename T::value_type>)
+                    special = false;
+            }
             if (!special)
             {
-                if (isContainer)
+                if constexpr (is_container_v<T>)
                     out += " array";
                 else
                     out += " scalar";
@@ -127,6 +174,24 @@ namespace mxTypes
         }
 
 
+        // forward declaration
+        template <typename T>
+        typename std::enable_if_t<!is_container_v<T>, bool>
+            checkInput_impl(const mxArray* inp_);
+        template <typename Cont>
+        typename std::enable_if_t<is_container_v<Cont> &&
+            !is_specialization_v<typename Cont::value_type, std::pair> &&
+            !is_specialization_v<typename Cont::value_type, std::tuple>,
+            bool>
+            checkInput_impl(const mxArray* inp_);
+        template<class Cont>
+        typename std::enable_if_t<
+            is_container_v<Cont> && (
+                is_specialization_v<typename Cont::value_type, std::pair> ||
+                is_specialization_v<typename Cont::value_type, std::tuple>),
+            bool>
+            checkInput_impl(const mxArray* inp_);
+        // end forward declaration
         template <typename T>
         bool checkInput_impl_cell(const mxArray* inp_)
         {
@@ -141,8 +206,62 @@ namespace mxTypes
 
             return true;
         }
+
+        template <template <class...> class TP, class... Args, size_t... Is>
+        bool checkInput_impl(const mxArray* inp_, TP<Args...>&&, std::index_sequence<Is...>, mwIndex iRow_ = 0, mwSize nRow_ = 1)
+        {
+            if (!mxIsCell(inp_))
+                return false;
+
+            return (checkInput_impl<Args>(mxGetCell(inp_, iRow_ + (Is)*nRow_))&& ...);
+        }
+        template<class Cont>
+        typename std::enable_if_t<
+            is_container_v<Cont> && (
+                is_specialization_v<typename Cont::value_type, std::pair> ||
+                is_specialization_v<typename Cont::value_type, std::tuple>),
+            bool>
+            checkInput_impl(const mxArray* inp_)
+        {
+            using theTuple = typename Cont::value_type;
+
+            // get info about input
+            auto nRow = mxGetM(inp_);
+            auto nCol = mxGetN(inp_);
+            if (nCol != std::tuple_size_v<theTuple>)
+                return false;
+
+            // per row, check each cell
+            for (mwIndex iRow = 0; iRow < nRow; ++iRow)
+                if (!checkInput_impl(inp_, theTuple(), std::make_index_sequence<std::tuple_size_v<theTuple>>{}, iRow, nRow))
+                    return false;
+
+            return true;
+        }
+        template <typename Cont>
+        typename std::enable_if_t<is_container_v<Cont> &&
+            !is_specialization_v<typename Cont::value_type, std::pair> &&
+            !is_specialization_v<typename Cont::value_type, std::tuple>,
+            bool>
+            checkInput_impl(const mxArray* inp_)
+        {
+            // early out for complex arguments, never wanted by us
+            if (mxIsComplex(inp_))
+                return false;
+
+            if constexpr (std::is_same_v<Cont, std::string>)
+                return mxIsChar(inp_);
+            else
+            {
+                if (typeNeedsMxCellStorage_v<Cont::value_type> || mxIsCell(inp_))
+                    return checkInput_impl_cell<Cont::value_type>(inp_);
+                else
+                    return checkInput_impl<Cont::value_type>(inp_);
+            }
+        }
         template <typename T>
-        bool checkInput_impl(const mxArray* inp_)
+        typename std::enable_if_t<!is_container_v<T>, bool>
+            checkInput_impl(const mxArray* inp_)
         {
             // early out for complex arguments, never wanted by us
             if (mxIsComplex(inp_))
@@ -150,22 +269,10 @@ namespace mxTypes
 
             // NB: below checks if mxArray contains exactly the expected type,
             // it does not check whether type could be acquired losslessly through a cast
-            if constexpr (std::is_same_v<T, std::string>)
-                return mxIsChar(inp_);
-            else if constexpr (is_container_v<T>)
-            {
-                if constexpr (typeNeedsMxCellStorage_v<T::value_type>)
-                    return checkInput_impl_cell<T::value_type>(inp_);
-                else
-                {
-                    if (mxIsCell(inp_))
-                        return checkInput_impl_cell<T::value_type>(inp_);
-                    else
-                        return mxGetClassID(inp_)==typeToMxClass_v<T::value_type>;
-                }
-            }
+            if constexpr (is_specialization_v<T, std::pair> || is_specialization_v<T, std::tuple>)
+                return checkInput_impl(inp_, T(), std::make_index_sequence<std::tuple_size_v<T>>{});
             else
-                return mxGetClassID(inp_)==typeToMxClass_v<T> && mxIsScalar(inp_);
+                return mxGetClassID(inp_) == typeToMxClass_v<T> && mxIsScalar(inp_);
         }
 
         template <typename T, typename T2>
@@ -203,34 +310,85 @@ namespace mxTypes
         }
 
 
-
+        // forward declarations
+        template <typename Cont>
+        typename std::enable_if_t<is_container_v<Cont> &&
+            !is_specialization_v<typename Cont::value_type, std::pair> &&
+            !is_specialization_v<typename Cont::value_type, std::tuple>,
+            Cont>
+            getValue_impl(const mxArray* inp_);
         template <typename T>
-        T getValue_impl(const mxArray* inp_)
+        typename std::enable_if_t<!is_container_v<T>, T>
+            getValue_impl(const mxArray* inp_);
+        // end forward declarations
+
+        template <template <class...> class TP, class... Args, size_t... Is>
+        TP<Args...> getValue_impl(const mxArray* inp_, TP<Args...>&&, std::index_sequence<Is...>, mwIndex iRow_ = 0, mwSize nRow_ = 1)
         {
-            if constexpr (std::is_same_v<T, std::string>)
+            if constexpr (is_specialization_v<TP<Args...>, std::tuple>)
+                return std::make_tuple(getValue_impl<Args>(mxGetCell(inp_, iRow_ + (Is)*nRow_)) ...);
+            else
+                return std::make_pair(getValue_impl<Args>(mxGetCell(inp_, iRow_ + (Is)*nRow_)) ...);
+        }
+        template<class Cont>
+        typename std::enable_if_t<
+                is_container_v<Cont> && (
+                    is_specialization_v<typename Cont::value_type, std::pair> ||
+                    is_specialization_v<typename Cont::value_type, std::tuple>),
+            Cont>
+            getValue_impl(const mxArray* inp_)
+        {
+            // get info about input (we've already checked that number of column is equal to tuple length)
+            auto nRow = mxGetM(inp_);
+
+            // per row, convert from cell
+            Cont out;
+            using theTuple = typename Cont::value_type;
+            for (mwIndex iRow = 0; iRow < nRow; ++iRow)
+                out.push_back(getValue_impl(inp_, theTuple(), std::make_index_sequence<std::tuple_size_v<theTuple>>{}, iRow, nRow));
+
+            return out;
+        }
+
+        template <typename Cont>
+        typename std::enable_if_t<is_container_v<Cont> && 
+                !is_specialization_v<typename Cont::value_type, std::pair> &&
+                !is_specialization_v<typename Cont::value_type, std::tuple>,
+            Cont>
+            getValue_impl(const mxArray* inp_)
+        {
+            if constexpr (std::is_same_v<Cont, std::string>)
             {
                 char* str = mxArrayToString(inp_);
-                T out = str;
+                Cont out = str;
                 mxFree(str);
                 return out;
             }
-            else if constexpr (is_container_v<T>)
+            else
             {
-                if (typeNeedsMxCellStorage_v<T::value_type> || mxIsCell(inp_))
+                if (typeNeedsMxCellStorage_v<Cont::value_type> || mxIsCell(inp_))
                 {
                     const auto nElem = static_cast<mwIndex>(mxGetNumberOfElements(inp_));
-                    T out;
+                    Cont out;
                     for (mwIndex i = 0; i < nElem; i++)
-                        out.emplace_back(getValue_impl<T::value_type>(mxGetCell(inp_, i)));
+                        out.emplace_back(getValue_impl<Cont::value_type>(mxGetCell(inp_, i)));
                     return out;
                 }
                 else
                 {
-                    auto data = static_cast<typename T::value_type*>(mxGetData(inp_));
+                    auto data = static_cast<typename Cont::value_type*>(mxGetData(inp_));
                     auto numel = mxGetNumberOfElements(inp_);
-                    return T(data, data + numel);
+                    return Cont(data, data + numel);
                 }
             }
+        }
+
+        template <typename T>
+        typename std::enable_if_t<!is_container_v<T>, T>
+            getValue_impl(const mxArray* inp_)
+        {
+            if constexpr (is_specialization_v<T, std::pair> || is_specialization_v<T, std::tuple>)
+                return getValue_impl(inp_, T(), std::make_index_sequence<std::tuple_size_v<T>>{});
             else
                 return *static_cast<T*>(mxGetData(inp_));
         }
