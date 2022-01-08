d6328ecd3faf6b53aec080b0f048ed725d590575
Diederick Niehorster <dcnieho@gmail.com>
2022-01-08 10:51:14 +0100

    DRY

--- invocable_traits.h
+++ invocable_traits.h
@@ -152,33 +152,34 @@ namespace detail
 #   undef MAKE_NOEXCEPT
 #   undef MAKE_VARIADIC
 
+    // specific overloaded operator() is available, use it for analysis
+    template <typename C, bool, typename... OverloadArgs>
+    struct invocable_traits_extract : invocable_traits_impl<std::decay_t<decltype(invocable_traits_resolve_overload<C, OverloadArgs...>(&C::operator()))>> {};
+
+    // unambiguous operator() is available, use it for analysis
+    template <typename C, bool B>
+    struct invocable_traits_extract<C, B> : invocable_traits_impl<decltype(&C::operator())> {};
+
     // check if passed type has an operator(), can be true for struct/class, includes lambdas
-    template <typename T>
-    concept HasCallOperator = requires(T t)
+    template <typename C>
+    concept HasCallOperator = requires(C t)
     {
         t.operator();
     };
-    // check if we can get operator(), will fail if overloaded (assuming above HasCallOperator does pass)
-    template <typename T>
+    // check if we can get operator(),
+    // will fail if not because overloaded (assuming above HasCallOperator does pass)
+    template <typename C>
     concept CanGetCallOperator = requires
     {
-        invocable_traits_impl<decltype(&T::operator())>();
+        invocable_traits_extract<C, true>();
     };
-
-    // check if we can get an operator() that takes the specified input arguments
+    // check if we can get an operator() that takes the specified input arguments,
+    // will fail only if not (assuming above HasCallOperator does pass)
     template <typename C, typename... OverloadArgs>
     concept HasSpecificCallOperator = requires
     {
-        invocable_traits_impl<std::decay_t<decltype(invocable_traits_resolve_overload<C, OverloadArgs...>(&C::operator()))>>();
+        invocable_traits_extract<C, true, OverloadArgs...>();
     };
-
-    // specific overloaded operator() is available, use it for analysis
-    template <typename T, bool, typename... OverloadArgs>
-    struct invocable_traits_extract : invocable_traits_impl<std::decay_t<decltype(invocable_traits_resolve_overload<T, OverloadArgs...>(&T::operator()))>> {};
-
-    // unambiguous operator() is available, use it for analysis
-    template <typename T, bool B>
-    struct invocable_traits_extract<T, B> : invocable_traits_impl<decltype(&T::operator())> {};
 
     // to reduce excessive compiler error output
     struct invocable_traits_error
